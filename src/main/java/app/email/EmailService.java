9e24e0f2f18577d5d15c65934c8dd3087b7aa208
VladimirShalamanov <vladi.gesh@gmail.com>
2025-10-14 16:45:08 +0300

    example with @Async

--- src/main/java/app/email/EmailService.java
+++ src/main/java/app/email/EmailService.java
@@ -2,14 +2,23 @@ package app.email;
 
 import app.event.SuccessfulChargeEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.annotation.Order;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
 
+    @Async
+    @Order(2)
     @EventListener
-    public  void sendEmail(SuccessfulChargeEvent event){
+    public void sendEmail(SuccessfulChargeEvent event) throws InterruptedException {
 
-        System.out.printf("Sending Email for new payment for user with [s%]", event.getEmail());
+        String threadName = Thread.currentThread().getName();
+        System.out.println("Thread in EmailService.java: " + threadName);
+
+        if (event.getEmail() != null) {
+            System.out.printf("Sending Email for new payment for user with [%s]", event.getEmail());
+        }
     }
 }
--- src/main/java/app/gift/GiftService.java
+++ src/main/java/app/gift/GiftService.java
@@ -2,13 +2,26 @@ package app.gift;
 
 import app.event.SuccessfulChargeEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.annotation.Order;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GiftService {
 
+    @Async
+    @Order(1)
     @EventListener
-    public void sendGift(SuccessfulChargeEvent event){
-        System.out.printf("Sending 1 EUR for charge compensation for the user with email [s%]", event.getEmail());
+    public void sendGift(SuccessfulChargeEvent event) {
+
+        String threadName = Thread.currentThread().getName();
+        System.out.println("Thread in GiftService.java: " + threadName);
+
+        if (event.getEmail() != null) {
+            System.out.printf("Sending 1 EUR for charge compensation for the user with email [%s]", event.getEmail());
+        }
     }
 }
+
+
+
